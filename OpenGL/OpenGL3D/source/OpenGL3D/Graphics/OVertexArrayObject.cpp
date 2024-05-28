6fa97b24f20cf406b13b8098251a3684c4084b0d
drktheprogramer <124218896+drktheprogramer@users.noreply.github.com>
2024-05-27 21:51:44 -0300

    Actualizar OVertexArrayObject.cpp

--- OpenGL/OpenGL3D/source/OpenGL3D/Graphics/OVertexArrayObject.cpp
+++ OpenGL/OpenGL3D/source/OpenGL3D/Graphics/OVertexArrayObject.cpp
@@ -5,6 +5,13 @@ OVertexArrayObject::OVertexArrayObject()
 {
  glGenBuffers(1,&m_vertexBufferId);
  glGenVertexArrays(1,&m_vertexArrayObjectId);
+
+ glBindVertexArray(m_vertexArrayObjectId);
+
+ glBindBuffer(GL_ARRAY_BUFFER,m_vertexBufferId);
+ glBufferData(GL_ARRAY_BUFFER,);
+
+ glBindVertexArray(0);
 }
 
 OVertexArrayObject::~OVertexArrayObject()
