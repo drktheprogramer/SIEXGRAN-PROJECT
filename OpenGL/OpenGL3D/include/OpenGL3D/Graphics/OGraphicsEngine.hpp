f3b0e0303e6edbd008e294fcc4ba3f1635b8676d
drktheprogramer <124218896+drktheprogramer@users.noreply.github.com>
2024-05-27 23:01:34 -0300

    Actualizar OGraphicsEngine.hpp

--- OpenGL/OpenGL3D/include/OpenGL3D/Graphics/OGraphicsEngine.hpp
+++ OpenGL/OpenGL3D/include/OpenGL3D/Graphics/OGraphicsEngine.hpp
@@ -10,6 +10,7 @@ OVertexArrayObjectPtr createArrayObject(const OVertexBufferData& data);
 	void clear(const OVec4& color);
  void setViewport(const ORect& size);
  void setVertexArrayObject(const OVertexArrayObjectPtr& vao);
+ void drawTriangles(ui32 vertexCount,ui32 offset);
  
 };
 
