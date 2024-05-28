ae2265a3260423a4ebdbbab2a104e0a66f11f446
drktheprogramer <124218896+drktheprogramer@users.noreply.github.com>
2024-05-27 22:36:19 -0300

    Actualizar OVertexArrayObject.hpp

--- OpenGL/OpenGL3D/include/OpenGL3D/Graphics/OVertexArrayObject.hpp
+++ OpenGL/OpenGL3D/include/OpenGL3D/Graphics/OVertexArrayObject.hpp
@@ -5,6 +5,7 @@ struct OVertexArrayObject {
  OVertexArrayObject(const OVertexBufferData& data);
  ~OVertexArrayObject();
 
+ ui32 getId();
 private:
  ui32 m_vertexBufferId=0;
  ui32 m_vertexArrayObjectId=0;
