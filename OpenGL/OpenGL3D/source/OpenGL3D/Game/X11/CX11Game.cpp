223b988485711dacffcda9d17d715fa640fac3b8
drktheprogramer <124218896+drktheprogramer@users.noreply.github.com>
2024-05-29 20:37:17 -0300

    Actualizar CX11Game.cpp

--- OpenGL/OpenGL3D/source/OpenGL3D/Game/X11/CX11Game.cpp
+++ OpenGL/OpenGL3D/source/OpenGL3D/Game/X11/CX11Game.cpp
@@ -22,9 +22,9 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.*/
 
-#include <OGame.h>
-#include <OWindow.h>
-#include <OGraphicsEngine.h>
+#include <OGame.hpp>
+#include <OWindow.hpp>
+#include <OGraphicsEngine.hpp>
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <CX11Globals.h>
